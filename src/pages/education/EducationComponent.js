bb880e27a0b9be0518aa46cd77afb95e5b716f0b
Mahesh Mokale <maheshmokale.mm@gmail.com>
2023-07-19 17:57:09 -0700

    Temporarily updated code to remove certifications

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import Educations from "../../containers/education/Educations";
-import Certifications from "../../containers/certifications/Certifications";
 import EducationImg from "./EducationImg";
 import { Fade } from "react-reveal";
 import "./EducationComponent.css";
@@ -23,20 +22,18 @@ function Education(props) {
                 Education
               </h1>
               <h3 className="heading-sub-text" style={{ color: theme.text }}>
-                Basic Qualification and Certifications
+                Basic Qualification
               </h3>
               <p
                 className="experience-header-detail-text subTitle"
                 style={{ color: theme.secondaryText }}
               >
-                I actively participate in tech-related activities. Below are
-                some of my major certifications.
+                I actively participate in tech-related activities.
               </p>
             </div>
           </div>
         </Fade>
         <Educations theme={props.theme} />
-        <Certifications theme={props.theme} />
       </div>
       <Footer theme={props.theme} />
     </div>
